828d372fb5cd570c3d82a17caf35279af4d98847
Richard Scott <rich5d@icloud.com>
2022-07-19 16:41:32 -0700

    Added Live Alert Button

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -36,6 +36,23 @@ function createCard(name, description, pictureUrl, newStartDate, newEndDate, con
             const pictureUrl = details.conference.location.picture_url;
 
             const conferenceLocation = details.conference.location.name;
+
+
+            var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+            var alertTrigger = document.getElementById('liveAlertBtn')
+
+            function alert(message, type) {
+            var wrapper = document.createElement('div')
+            wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+            alertPlaceholder.append(wrapper)
+            }
+
+            if (alertTrigger) {
+            alertTrigger.addEventListener('click', function () {
+                alert('Nice, you triggered this alert message!', 'success')
+            })
+            }
             
 
             const startDate = details.conference.starts;
