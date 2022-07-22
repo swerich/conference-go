c0508ec0a52478f39a5a039d1a1aaf91d519a8dc
Richard Scott <rich5d@icloud.com>
2022-07-21 17:20:05 -0700

    Prior to adding signup

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -17,6 +17,34 @@ window.addEventListener('DOMContentLoaded', async () => {
       loadTag.classList.add('d-none')
       selectTag.classList.remove('d-none')
 
+
+      
+      const formTag = document.getElementById('create-attendee-form')
+        formTag.addEventListener('submit', async(event) => {
+        event.preventDefault()
+        
+        const formData = new FormData(formTag)
+        const json = JSON.stringify(Object.fromEntries(formData))
+        
+        
+        const attendeeUrl = 'http://localhost:8001/api/attendees/'
+        const fetchConfig = {
+            method: "POST",
+            body: json,
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          const response = await fetch(attendeeUrl, fetchConfig);
+          if (response.ok) {
+            formTag.reset()
+            const newAttendee = await response.json()
+            console.log(newAttendee)
+
+            const loadTag = document.getElementById('success-message');
+            loadTag.classList.add('d-none')
+            selectTag.classList.remove('d-none')
+        }
+        })
     }
-  
-  });
+});
